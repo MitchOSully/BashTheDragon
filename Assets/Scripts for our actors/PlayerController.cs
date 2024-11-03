67e3db707064f388f4b398674635a8cb390535f0
MitchOSully <mitchell.osullivan@live.com.au>
2024-11-03 15:04:03 +0800

    Player faces the way he's moving
    - Also deleted redundant images of Yharon the dragon

--- Assets/Scripts for our actors/PlayerController.cs
+++ Assets/Scripts for our actors/PlayerController.cs
@@ -10,22 +10,44 @@ public class PlayerController : MonoBehaviour
   public Transform groundCheck;
   public LayerMask groundLayer;
 
+  private bool bFacingRight = true;
+
     void Update()
     {
-        rb.velocity = new Vector2(Input.GetAxis("Horizontal") * speed, rb.velocity.y);
-
-        if (IsGrounded())
-        {
-          //Jumping
-          if (Input.GetButtonDown("Jump"))
-          {
-            rb.velocity = new Vector2(rb.velocity.x, jumpPower);
-          }
-        }
+      UpdateVelocity();
+      UpdateSprite();
     }
 
     private bool IsGrounded()
     {
       return Physics2D.OverlapCircle(groundCheck.position, 0.2f, groundLayer);
+    }
+
+    private void UpdateVelocity()
+    {
+      rb.velocity = new Vector2(Input.GetAxis("Horizontal") * speed, rb.velocity.y);
+
+      if (IsGrounded())
+      {
+        //Jumping
+        if (Input.GetButtonDown("Jump"))
+        {
+          rb.velocity = new Vector2(rb.velocity.x, jumpPower);
+        }
+      }
+    }
+
+    private void UpdateSprite()
+    {
+      if (rb.velocity.x > 0 && !bFacingRight)
+        Flip();
+      else if (rb.velocity.x < 0 && bFacingRight)
+        Flip();
+    }
+
+    private void Flip()
+    {
+      transform.localScale = new Vector3 (-transform.localScale.x, transform.localScale.y, transform.localScale.z);
+      bFacingRight = !bFacingRight;
     }
 }
