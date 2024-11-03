198c7d5b2395bca39e2b65fc22ed9f1122ae69ea
MitchOSully <mitchell.osullivan@live.com.au>
2024-11-03 14:41:27 +0800

    Press 'R' to restart + Fix missing script warning

--- Assets/Scripts for our actors/theWASlimetationsWASandWASpowersWASthatWAStheDMessentualWAScontanes.cs
+++ Assets/Scripts for our actors/theWASlimetationsWASandWASpowersWASthatWAStheDMessentualWAScontanes.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 using TMPro;
 
 public class theWASlimetationsWASandWASpowersWASthatWAStheDMessentualWAScontanes : MonoBehaviour
@@ -17,7 +18,10 @@ public class theWASlimetationsWASandWASpowersWASthatWAStheDMessentualWAScontanes
     // Update is called once per frame
     void Update()
     {
-        
+        if (Input.GetKeyDown(KeyCode.R))
+        {
+            SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+        }
     }
 
     public void DisplayMessage(uint index)
